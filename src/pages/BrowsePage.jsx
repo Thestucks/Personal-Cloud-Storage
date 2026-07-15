d13a40481b58e393a1468c85733ec5a25e71d45c
Thestucks <thestucks@users.noreply.github.com>
2026-07-15 15:39:59 +0700

    Update BrowsePage.jsx

--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams, useSearchParams, Link } from 'react-router-dom'
 import { useAccount } from '../hooks/useAccounts'
 import { useFiles } from '../hooks/useFiles'
